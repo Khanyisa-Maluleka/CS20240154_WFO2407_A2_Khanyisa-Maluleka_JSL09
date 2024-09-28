01b206b98462053d465dc0bfdaf5e484f5c7f783
Khanyisa Maluleka <khanyisamaluleka20@gmail.com>
2024-09-28 22:30:40 +0200

    Fixed up my JS

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ try {
     document.getElementById("author").textContent = `By: Dodi Achmad`
 }
 
-
 try {
     const res = await fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
     if (!res.ok) {
